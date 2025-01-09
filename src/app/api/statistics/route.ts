ef347b7bbf6d945e612cb6c61522d6966a25be1e
tsvetomir.tsekov <tsvetomir.tsekov@scalehub.com>
2025-01-09 23:10:35 +0200

    refactor statistics API response handling to improve type safety by specifying expected response structure

--- src/app/api/statistics/route.ts
+++ src/app/api/statistics/route.ts
@@ -23,7 +23,7 @@ export async function GET(request: Request) {
 
   try {
     const response = await fetch(url, options);
-    const result = await response.json();
+    const result = (await response.json()) as { response: Statistics[] };
 
     if (
       !result.response ||
