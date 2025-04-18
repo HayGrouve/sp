62527fe30ea7a4ebf3ff3700668c07ff10c9b435
tsvetomir.tsekov <tsvetomir.tsekov@scalehub.com>
2025-04-18 18:48:59 +0300

    refactor: integrate next-themes for theme management and enhance Tailwind CSS configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import { TanstackProvider } from "@/providers/tanstack-provider";
+import { ThemeProvider } from "../providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,9 +16,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <TanstackProvider>{children}</TanstackProvider>
+        <TanstackProvider>
+          <ThemeProvider
+            attribute="class" // <-- VERY IMPORTANT: Tells next-themes to use class strategy
+            defaultTheme="system" // Or "light" or "dark"
+            enableSystem // Allows theme to follow system preference
+            disableTransitionOnChange // Optional: Prevents theme flash on load
+          >
+            {children}
+          </ThemeProvider>
+        </TanstackProvider>
       </body>
     </html>
   );
